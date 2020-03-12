4b46839f3cba755ab63da8e94ef8880f35676917
NEEMIAS DA SILVA SOUZA <neemias.souza@corenmg.gov.br>
2020-03-12 14:57:56 -0300

    final version 100% uhu

--- src/main/java/com/cesar/decrypt/resource/Decrypter.java
+++ src/main/java/com/cesar/decrypt/resource/Decrypter.java
@@ -1,19 +1,10 @@
 package com.cesar.decrypt.resource;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.net.MalformedURLException;
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
 import org.springframework.util.LinkedMultiValueMap;
@@ -27,43 +18,40 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.cesar.decrypt.model.AnswerDTO;
 import com.google.gson.Gson;
 
-import reactor.core.publisher.Flux;
-
 @RestController
 @RequestMapping(value = "/cesar")
 public class Decrypter {
 
 	static final String alphabet = "abcdefghijklmnopqrstuvxwyz";
-	File answer = new File("C:\\Users\\neemi\\Documents\\workspace-sts-3.9.9.RELEASE\\CesarsDecoder\\answer.json");
+	File answer = new File("C:\\Users\\neemias.souza\\Documents\\rkSpcGiWt\\back\\CodeNation_CesarsCode\\answer.json");
 	static Gson gson = new Gson();
 
 	@GetMapping
-	public void getStarterJson() throws Exception {
-		
-		AnswerDTO answer = WebClient.create()
-        .get().uri("https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=8eab8d5fdd5b05e5b1411dd532aa2aa29f2ec0ce")
-        .retrieve().bodyToMono(AnswerDTO.class)
-        .block();
-	
+	public String getStarterJson() throws Exception {
+
+		AnswerDTO answer = WebClient.create().get().uri(
+				"https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=8eab8d5fdd5b05e5b1411dd532aa2aa29f2ec0ce")
+				.retrieve().bodyToMono(AnswerDTO.class).block();
+
 		save(this.answer, answer);
-		
-		//Descriptografando
+
+		// Descriptografando
 		String decryptedText = this.decrypter(answer.getCifrado(), answer.getNumero_casas());
 		answer.setDecifrado(decryptedText);
-		
-		//Resumo Criptografico
+
+		// Resumo Criptografico
 		answer.setResumo_criptografico(setCryptographicResume(answer.getDecifrado()));
-			
+
 		save(this.answer, answer);
-		
-		submistAnswer();
+
+		return submitAnswer();
 
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	public void save(File file, AnswerDTO answer) throws Exception {
 		String json = gson.toJson(answer); // Convertendo resposta para string em json
-		FileUtils.writeStringToFile(file, json); //Salvando o arquivo
+		FileUtils.writeStringToFile(file, json); // Salvando o arquivo
 	}
 
 	public String decrypter(String encryptedText, Integer numCasas) throws Exception {
@@ -84,84 +72,49 @@ public class Decrypter {
 
 			else if (alphabet.indexOf(charToBeUncrypted) - numCasas < 0) {
 				decoded += alphabet.toCharArray()[alphabet.length()
-				                                  + ((alphabet.indexOf(charToBeUncrypted) - numCasas))] + "";
+						+ ((alphabet.indexOf(charToBeUncrypted) - numCasas))] + "";
 			} else {
 
 				if (charToBeUncrypted != ' ') {
-					decoded += alphabet.toCharArray()[alphabet.toString()
-					                                  .indexOf((charToBeUncrypted) - numCasas)] + "";
+					decoded += alphabet.toCharArray()[alphabet.toString().indexOf((charToBeUncrypted) - numCasas)] + "";
 				}
 			}
 		}
 
 		return decoded; // Setando o texto frase descriptografada
-	
+
 	}
-	
+
 	public String setCryptographicResume(String decryptedText) throws Exception {
-		
-		MessageDigest digest =  MessageDigest.getInstance("SHA-1"); 
-		byte[] byteArray = digest.digest(decryptedText.getBytes(StandardCharsets.UTF_8));
-		
+
+		MessageDigest digest = MessageDigest.getInstance("SHA-1");
+		byte[] byteArray = digest.digest(decryptedText.getBytes());
 
 		StringBuilder cryptographicResume = new StringBuilder();
-		
+
 		for (int i = 0; i < byteArray.length; i++) {
-			String hex = Integer.toHexString(0xff & byteArray[i]);
-			
-			hex = hex.length() == 1 ? "0" : hex;
+			String hex = Integer.toString((byteArray[i] & 0xff) + 0x100, 16).substring(1);
+
 			cryptographicResume.append(hex);
 		}
-		
+
 		return cryptographicResume.toString();
 	}
-	
-	private void submistAnswer() throws Exception {
-		
-		WebClient.create().post().contentType(MediaType.MULTIPART_FORM_DATA).retrieve();
 
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		HttpPost uploadFile = new HttpPost("https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=8eab8d5fdd5b05e5b1411dd532aa2aa29f2ec0ce");
-		MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-		builder.addTextBody("answer", "answer", ContentType.MULTIPART_FORM_DATA);
+	public String submitAnswer() throws MalformedURLException {
 
-		
-		// Anexando o arquivo ao post:
-		builder.addBinaryBody( "answer", new FileInputStream(answer), ContentType.MULTIPART_FORM_DATA, answer.getName());
-
-		HttpEntity multipart = builder.build();
-		uploadFile.setEntity(multipart);
-		
-		CloseableHttpResponse response = httpClient.execute(uploadFile);
-		HttpEntity responseEntity = response.getEntity();
-		responseEntity.getContent();
-	
-	}
-	
-	//Not working
-	public Flux<String> submitAnswer() throws MalformedURLException {
-	    
 		final UrlResource resource = new UrlResource(
-				"file:///C:\\Users\\neemi\\Documents\\workspace-sts-3.9.9.RELEASE\\CesarsDecoder\\answer.json");
+				"file://C:\\Users\\neemias.souza\\Documents\\rkSpcGiWt\\back\\CodeNation_CesarsCode\\answer.json");
 
-	    MultiValueMap<String, UrlResource> data = new LinkedMultiValueMap<>();
-	    data.add("answer", resource);
+		MultiValueMap<String, UrlResource> data = new LinkedMultiValueMap<>();
+		data.add("answer", resource);
 
-	    return WebClient.create().post()
-	            .uri("https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=8eab8d5fdd5b05e5b1411dd532aa2aa29f2ec0ce")
-	            .contentType(MediaType.MULTIPART_FORM_DATA)
-	            .body(BodyInserters.fromMultipartData(data))
-	            .exchange()
-	            .flatMap(response -> response.bodyToMono(String.class))
-	            .flux();
+		String retorno = WebClient.create().post().uri(
+				"https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=8eab8d5fdd5b05e5b1411dd532aa2aa29f2ec0ce")
+				.contentType(MediaType.MULTIPART_FORM_DATA).body(BodyInserters.fromMultipartData(data)).retrieve()
+				.bodyToMono(String.class).block();
+
+		return retorno;
 	}
-	
-	public void submitsAnswerteste() {
-		
-//		RestClient client = new RestClient("url");
-//		RestRequest requests = new RestRequest(HttpMethod.POST);
-//		requests.AddHeader("Content-Type", "multipart/form-data;");
-//		requests.AddFile("answer", "path");
-	}
-	
+
 }
